44109860342008af5b8865ccd4716ad2112ac980
Wes <wesley.nimmo@lauruscollege.edu>
2025-05-13 14:18:08 -0700

    attempting to fix header and sidebar

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
-const body = document.body;
-const lightIcon = document.getElementById("lightModeIcon");
-const darkIcon = document.getElementById("darkModeIcon");
-const menuToggle = document.getElementById("menuToggle");
-const sidebar = document.getElementById("sidebar");
+const menuToggle = document.getElementById('menuToggle');
+const sidebar = document.getElementById('sidebar');
 
-// Dark/light mode
-lightIcon.addEventListener("click", () => {
-  body.classList.remove("dark");
-  body.classList.add("light");
-});
-
-darkIcon.addEventListener("click", () => {
-  body.classList.remove("light");
-  body.classList.add("dark");
-});
-
-// Sidebar toggle on mobile
-menuToggle.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
+menuToggle.addEventListener('click', () => {
+  sidebar.classList.toggle('open');
 });
